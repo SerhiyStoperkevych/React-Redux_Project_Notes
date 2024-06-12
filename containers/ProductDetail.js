fe709bbdc9e03a47403311ade926fe5082cb2dee
SerhiyStoperkevych <116464169+SerhiyStoperkevych@users.noreply.github.com>
2024-06-12 14:42:13 -0400

    Create ProductDetail.js

---
+++ containers/ProductDetail.js
@@ -0,0 +1,8 @@
+import reducers from "../redux/reducers";
+import React from 'react';
+
+const ProductDetail = () => {
+  return(<div><h1>ProductDetail</h1></div>)
+}
+
+export default ProductDetail;
